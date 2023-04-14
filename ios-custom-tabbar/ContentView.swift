e0f1a1ee57387b4f24c8762c0b5f2b5458d5d0c2
tonezone6 <tonezone6@gmail.com>
2023-04-14 09:40:43 +0200

    Initial Commit

---
+++ ios-custom-tabbar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios-custom-tabbar
+//
+//  Created by Alex S. on 14/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-custom-tabbar/ios_custom_tabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_custom_tabbarApp.swift
+//  ios-custom-tabbar
+//
+//  Created by Alex S. on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_custom_tabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
